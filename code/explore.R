79e73237a2c0bfd3a9410f706fd75410f634f002
positivebob <positivebob@gmail.com>
2018-04-05 13:09:56 -0400

    working on file subsetting routines

--- code/explore.R
+++ code/explore.R
@@ -15,7 +15,7 @@ library(knitr)
 # #install.packages("tidyr")
 # library(tidyr)
 # #install.packages("sp")
-# library(sp)
+library(sp)
 # #install.packages("maptools")
 # library(maptools)
 # #install.packages("maps")
@@ -63,17 +63,12 @@ library(extrafont)
 library(RColorBrewer)
 # library(googlesheets)
 # library(googledrive)
-#from csv
-setwd("C:/rworking/digs/indata")
-indata<-read.csv("DSCRTP_NatDB_20180327-4.csv", header = T)
-filt <- indata %>%
-  filter(Flag == "0")
-
+library(arcgisbinding)
 
 ##### load input data #####
-# latest working copy of NOAA National Database for Deep Sea Corals and Sponges
-# setwd("C:/rworking/digs/indata")
-# indata<-read.csv("DSCRTP_NatDB_20180327-4.csv", header = T)
+#latest working copy of NOAA National Database for Deep Sea Corals and Sponges
+setwd("C:/rworking/digs/indata")
+indata<-read.csv("DSCRTP_NatDB_20180327-4.csv", header = T)
 
 ##### filter data #####
 
@@ -81,28 +76,74 @@ filt <- indata %>%
 filt <- indata %>%
   filter(Flag == "0")
 
-# further filtering to focus on AOI 
-x <- indata %>% 
+# further filtering to focus on specific AOIs
+geo <- filt %>% 
   filter(
     Latitude > 29,
     Latitude < 52,
     Longitude < -110,
     Longitude > -132,
-    DatasetID != 'MBARI'
-    # ScientificName == 'Flabellum sp.'
   )
+
+mbari <- geo %>% 
+  filter(
+    DatasetID == 'MBARI'
+  )
+
+no_mbari <- geo %>% 
+  filter(
+    DatasetID != 'MBARI',
+    is.na(SamplingEquipment) == T,
+    EndLatitude == '-999'
+  )
+
+
+
+table(factor(mbari$SamplingEquipment), useNA = 'always')
+table(factor(no_mbari$SamplingEquipment), useNA = 'always')
+table(factor(no_mbari$DepthMethod), useNA = 'always')
+table(factor(no_mbari$RecordType), useNA = 'always')
+
+
+##### _____ creating spatialpointsdataframe from CSV#####
+# defining coordinates
+x <- geo
+coords <- subset(x, select = c("Longitude", "Latitude"))
+
+# making coords vectors numeric
+coords$Latitude<-as.numeric(coords$Latitude)
+coords$Longitude<-as.numeric(coords$Longitude)
+
+# creating SpatialPointsDataFrame from the subset. 
+sdf<-SpatialPointsDataFrame(coords, x, 
+                             proj4string = CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"), 
+                             match.ID = TRUE)
+
+##### _____ transferring sp file to arc object ##### 
+library(arcgisbinding)
+arc.check_product()
+
+#### turning back into ArcGIS object ##### 
+arcsdf <- arc.sp2data(sdf)
+
+##### writing back out to a feature class #####
+arc.write("c:\\data\\aptx\\20180405_0_West_Coast_Science_Planning_Maps_RPMcGuinn\\mmmh.gdb\\yo_what", 
+          arcsdf)
+
+# C:\data\aptx\20180405_0_West_Coast_Science_Planning_Maps_RPMcGuinn
+
 
 ##### _____ leaflet map #####
 # making a predined color palette based on a certain domain of values
 
 pal <- colorFactor(
   palette = 'Dark2',
-  domain = x$Flag
+  domain = no_mbari$Flag
 )
 
 # making a leaflet map and coloring by the predefined palette above 
 library(leaflet)
-leaflet(x) %>% 
+leaflet(no_mbari) %>% 
   addProviderTiles("Esri.OceanBasemap") %>% 
   addCircleMarkers(radius=5, 
                    weight=0, 
@@ -126,32 +167,6 @@ leaflet(x) %>%
                      "<b><em>","Image:","</b></em>",x$ImageURL)
                    
   )
-
-
-##### _____ creating spatialpointsdataframe #####
-# defining coordinates
-coords <- subset(x, select = c("Longitude", "Latitude"))
-
-# making coords vectors numeric
-coords$Latitude<-as.numeric(coords$Latitude)
-coords$Longitude<-as.numeric(coords$Longitude)
-
-# creating SpatialPointsDataFrame from the subset. 
-sdf<-SpatialPointsDataFrame(coords, x, 
-                             proj4string = CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"), 
-                             match.ID = TRUE)
-
-##### _____ setting up the arc to R bridge ##### 
-library(arcgisbinding)
-arc.check_product()
-
-#### making it back to an ArcGIS  object ##### 
-arcsdf <- arc.sp2data(sdf)
-
-##### writing back out to a shape file #####
-arc.write("c:\\data\\baselayers\\arc2Rtest", arcsdf)
-
-
 
 
 
