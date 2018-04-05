a6d270e67598b8070f3c81c5e5d17367c0fb2022
positivebob <positivebob@gmail.com>
2018-04-05 15:13:45 -0400

    added code to extract records with underlying linear geometry for conversion to shapefile

--- code/explore.R
+++ code/explore.R
@@ -39,7 +39,7 @@ library(dplyr)
 # #install.packages("digest")
 # library(digest)
 # #install.packages("rgdal")
-# library(rgdal)
+library(rgdal)
 # #install.packages("ggmap")
 # library(ggmap)
 # #install.packages("rerddap")
@@ -97,12 +97,23 @@ no_mbari <- geo %>%
     EndLatitude == '-999'
   )
 
+lines <- geo %>% 
+  filter(
+     EndLatitude != '-999'
+  )
+
+lines$depthdiff <- lines$EndLatitude - lines$StartLatitude
+plot(lines$depthdiff)
 
 
 table(factor(mbari$SamplingEquipment), useNA = 'always')
 table(factor(no_mbari$SamplingEquipment), useNA = 'always')
 table(factor(no_mbari$DepthMethod), useNA = 'always')
 table(factor(no_mbari$RecordType), useNA = 'always')
+table(factor(lines$EndLatitude), useNA = 'always')
+table(factor(lines$EndLatitude), useNA = 'always')
+table(factor(lines$DataProvider), useNA = 'always')
+table(factor(lines$RecordType), useNA = 'always')
 
 
 ##### _____ creating spatialpointsdataframe from CSV#####
@@ -134,6 +145,9 @@ arc.write("c:\\data\\aptx\\20180405_0_West_Coast_Science_Planning_Maps_RPMcGuinn
 
 
 ##### _____ leaflet map #####
+# setting up the proper input data
+x <- no_mbari
+
 # making a predined color palette based on a certain domain of values
 
 pal <- colorFactor(
@@ -142,6 +156,8 @@ pal <- colorFactor(
 )
 
 # making a leaflet map and coloring by the predefined palette above 
+
+
 library(leaflet)
 leaflet(no_mbari) %>% 
   addProviderTiles("Esri.OceanBasemap") %>% 
@@ -170,3 +186,95 @@ leaflet(no_mbari) %>%
 
 
 
+
+
+##### working on mapping lines ##### 
+##### make shapefile lines from xy coordinates and mapping with leaflet and points #####
+setwd("C:/rworking/digs/indata")
+
+x <- "DSCRTP_NatDB_OceanaSubmission_SouthernCA2016_points"
+d2 <- read.csv(paste(x,".csv",sep=''),header = T)
+
+x <- "DSCRTP_NatDB_OceanaSubmission_SouthernCA2016_lines"
+d <- read.csv(paste(x,".csv",sep=''),header = T)
+
+begin.coord <- data.frame(lon=d$StartLongitude, lat=d$StartLatitude)
+end.coord <- data.frame(lon=d$EndLongitude, lat=d$EndLatitude)
+
+l <- vector("list", nrow(begin.coord))
+library(sp)
+for (i in seq_along(l)) {
+  l[[i]] <- Lines(list(Line(rbind(begin.coord[i, ], end.coord[i,]))), as.character(i))
+}
+lines<-SpatialLines(l)
+class(lines)
+
+##### map lines and points using leaflet #####
+library(leaflet)
+m <- leaflet()
+m <- addProviderTiles(m, "Esri.OceanBasemap") #Esri.OceanBasemap, "CartoDB.DarkMatter"
+m <- addPolylines(m, data=lines,
+                  fillColor= "green"
+                  
+)
+m <- addCircleMarkers(m, data=d2, 
+                      radius=1, 
+                      weight=0,
+                      lng = d2$LongitudeInDD, 
+                      lat = d2$LatitudeInDD,
+                      fillColor= "red", 
+                      fillOpacity=1,
+                      popup = d$ScientificName
+)
+m
+
+##### getting line segments and calculatng length #####
+# Getting just the records with not '-999' in the start and end positions.  
+d <- geo %>%
+  filter(
+    StartLatitude != "-999", 
+    StartLongitude != "-999", 
+    EndLatitude != "-999", 
+    EndLongitude != "-999"
+  )
+
+# #checking
+# d <- head(d, n = 2000)
+# length(d$CatalogNumber)
+
+d2 <- d #%>%
+#filter(StartLatitude != "-999")
+coordinates(d2) <- c("Longitude", "Latitude")
+proj4string(d2) <- "+proj=longlat +ellps=WGS84 +datum=WGS84"
+
+# creating spatial lines
+begin.coord <- data.frame(lon=d2$StartLongitude, lat=d2$StartLatitude)
+end.coord <- data.frame(lon=d2$EndLongitude, lat=d2$EndLatitude)
+
+l <- vector("list", nrow(begin.coord))
+library(sp)
+for (i in seq_along(l)) {
+  l[[i]] <- Lines(list(Line(rbind(begin.coord[i, ], end.coord[i,]))), as.character(i))
+}
+lines<-SpatialLines(l)
+class(lines)
+
+# this step turns the lines file into a SpatialLinesDataFrame for use with the writeOGR function
+lines2 <- SpatialLinesDataFrame(lines, d2@data)
+# class(lines2)
+# names(lines2)
+
+# adding a length variable
+lines2$length <- SpatialLinesLengths(lines, longlat = T)
+proj4string(lines2) <- "+proj=longlat +ellps=WGS84 +datum=WGS84"
+# lines2
+# names(lines2)
+
+setwd("C:/rworking/digs/outdata")
+writeOGR(lines2, dsn="what", 
+         layer= "what", 
+         driver = "ESRI Shapefile",
+         dataset_options=c("NameField=CatalogNumber"), 
+         overwrite_layer = T)
+
+# summary(lines2$length)
